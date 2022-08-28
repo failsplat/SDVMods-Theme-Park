c189eea2e21990ab1468a207fe9d3c75bdbe3854
failsplat <68579200+failsplat@users.noreply.github.com>
2022-08-28 00:31:50 -0400

    Minigame Testing command

--- SDV-ThemePark C#/SDV-ThemePark/ModEntry.cs
+++ SDV-ThemePark C#/SDV-ThemePark/ModEntry.cs
@@ -16,6 +16,59 @@ namespace SDV_ThemePark
     {
         public override void Entry(IModHelper helper)
         {
+            helper.ConsoleCommands.Add("minigame_run", "Starts a minigame from the Theme Park mod.", this.TestGame);
         }
+
+        private void TestGame(string command, string[] args)
+        {
+            if (!Context.IsPlayerFree) {
+                Monitor.Log("To run a minigame Must be in a loaded game and not busy!", LogLevel.Warn);
+                return;
+            }
+
+            int i = 0;
+            string gameName;
+            bool enteredNumber = int.TryParse(args[0], out i);
+            if (enteredNumber) {
+                if (gameNumbers.ContainsKey(i))
+                {
+                    gameName = gameNumbers[i];
+                }
+                else
+                {
+                    Monitor.Log($"Did not find a minigame numbered: {i}!", LogLevel.Info);
+                    return;
+                }
+            }
+            else {
+                gameName = args[0];
+            }
+
+            if (minigameDict.ContainsKey(gameName))
+            {
+                Monitor.Log($"Running minigame: \"{gameName}\"!", LogLevel.Info);
+                Game1.currentMinigame = Activator.CreateInstance(minigameDict[gameName]) as StardewValley.Minigames.IMinigame;
+
+            } 
+            else
+            {   
+                if (enteredNumber) {
+                    Monitor.Log($"Did not find a minigame named: {gameName} (#{i})!", LogLevel.Info);
+                } else {
+                    Monitor.Log($"Did not find a minigame named: {gameName}!", LogLevel.Info); 
+                }
+            }
+        }
+
+        private static readonly Dictionary<int, string> gameNumbers = new Dictionary<int, string>()
+        {
+            {0, "shell"},
+        };
+
+        private static readonly Dictionary<string, Type> minigameDict = new Dictionary<string, Type>()
+        {
+
+        };
+
     }
 }
